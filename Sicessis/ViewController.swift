f9863368634de6f3b9fd04be37c8b237749a8709
ramses7083 <ramses7083@gmail.com>
2017-07-14 16:33:45 -0500

    Initial Commit

---
+++ Sicessis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sicessis
+//
+//  Created by Ramses Miramontes Meza on 14/07/17.
+//  Copyright © 2017 Ramses Miramontes Meza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
